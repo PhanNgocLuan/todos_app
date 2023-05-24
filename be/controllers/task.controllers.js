76b8dfc9482e2cc575f9480a853542d4aa9151d4
PhanNgocLuan <56753332+PhanNgocLuan@users.noreply.github.com>
2023-05-24 16:26:37 +0700

    Update task.controllers.js

--- be/controllers/task.controllers.js
+++ be/controllers/task.controllers.js
@@ -22,11 +22,11 @@ exports.updateTask = async (req, res, next) => {
   const { id } = req.params;
   const { task_name } = req.body;
   try {
-     const task = await taskService.updateTask(id, task_name);
-     res.status(200).json(task);
-   } catch (error) {
-     next(error);
-   }
+    const task = await taskService.updateTask(id, task_name);
+    res.status(200).json(task);
+  } catch (error) {
+    next(error);
+  }
 };
 exports.deleteTask = async (req, res, next) => {
   const { id } = req.params;
@@ -35,5 +35,15 @@ exports.deleteTask = async (req, res, next) => {
     res.status(200).send("Task has been delete");
   } catch (error) {
     next(error);
+  }
+};
+
+exports.searchTask = async (req, res) => {
+  const { keyword } = req.query.keyword;
+  try {
+    const tasks = await taskService.searchTask(keyword);
+    res.status(200).json(tasks);
+  } catch (error) {
+    res.status(500).json({ error: error });
   }
 };
